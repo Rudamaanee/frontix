2d1c2896cdf7641a4ec6fb704546e1eeed9df7c1
Happy Ruda <01036156791@nate.com>
2026-03-10 15:45:12 +0900

    ui

--- js/main.js
+++ js/main.js
@@ -1,39 +1,63 @@
 fetch('./datas/project.json')
-    .then(res=>res.json())
-    .then(data=>{
-        const list=document.getElementById('projects')
-        data.forEach(item=>{
-            const html=`
-                <div class="c-card">
-                <span class="c-badge">${item.company}</span>
-                <h4>${item.project}</h4>
-                <p>${item.period}</p>
-                <p>${item.tech}</p>
-                </div>
 
-            `
-            list.insertAdjacentHTML('beforeend',html)
-        })
+.then(res=>res.json())
+
+.then(data=>{
+
+const list=document.getElementById('projects')
+
+data.forEach(item=>{
+
+const html=`
+
+<div class="project">
+
+<h4>${item.company}</h4>
+
+<p>${item.project}</p>
+
+<p>${item.period}</p>
+
+<p>${item.tech}</p>
+
+</div>
+
+`
+
+list.insertAdjacentHTML('beforeend',html)
+
+})
+
 })
 
 
 
 fetch('./datas/history.json')
-    .then(res=>res.json())
-    .then(data=>{
-        const list=document.getElementById('history')
-        data.forEach(item=>{
-            const html=`
-                <div class="c-card">
 
-                <span class="c-badge">${item.year}</span>
+.then(res=>res.json())
 
-                <h4>${item.company}</h4>
+.then(data=>{
 
-                <p>${item.project}</p>
+const list=document.getElementById('history')
 
-                </div>
-            `
-            list.insertAdjacentHTML('beforeend',html)
-        })
+data.forEach(item=>{
+
+const html=`
+
+<div class="timeline-item">
+
+<strong>${item.year}</strong>
+
+<p>${item.company}</p>
+
+<p>${item.project}</p>
+
+</div>
+
+`
+
+list.insertAdjacentHTML('beforeend',html)
+
+})
+
 })
