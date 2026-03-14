5055111891caae06793e10bf6d992748a0168e44
Happy Ruda <01036156791@nate.com>
2026-03-14 15:01:02 +0900

    history

--- js/main.js
+++ js/main.js
@@ -1,63 +1,41 @@
 fetch('./datas/project.json')
-
 .then(res=>res.json())
-
 .then(data=>{
-
 const list=document.getElementById('projects')
+    data.forEach(item=>{
+    const html=`
+        <div class="project-wrap">
+            <h4>${item.company}</h4>
+            <p>${item.project}</p>
+            <p>${item.period}</p>
+            <p>${item.tech}</p>
+        </div>
+    `
+    list.insertAdjacentHTML('beforeend',html)
+    })
+}) //project
 
-data.forEach(item=>{
+fetch('./datas/timeline.json')
+    .then(res => res.json())
+    .then(data => {
+        const container = document.getElementById('timeline')
+        data.forEach(yearData => {
+        let yearHtml = `
+            <swiper-slide class="timeline-year">
+            <div class="year"><span>${yearData.year}</span></div>
+            <div class="item-wrap">
+        `
+        yearData.items.forEach(item => {
+            yearHtml += `
+            <div class="timeline-item">
+                <div class="logo ${item.class}">${item.client}</div>
+                <p class="project">${item.project}</p>
+                <p class="tech">${item.tech}</p>
+            </div>
+            `
+        })
+        yearHtml += `</div></swiper-slide>`
+        container.insertAdjacentHTML('beforeend', yearHtml)
+    })
+}) //timeline
 
-const html=`
-
-<div class="project">
-
-<h4>${item.company}</h4>
-
-<p>${item.project}</p>
-
-<p>${item.period}</p>
-
-<p>${item.tech}</p>
-
-</div>
-
-`
-
-list.insertAdjacentHTML('beforeend',html)
-
-})
-
-})
-
-
-
-fetch('./datas/history.json')
-
-.then(res=>res.json())
-
-.then(data=>{
-
-const list=document.getElementById('history')
-
-data.forEach(item=>{
-
-const html=`
-
-<div class="timeline-item">
-
-<strong>${item.year}</strong>
-
-<p>${item.company}</p>
-
-<p>${item.project}</p>
-
-</div>
-
-`
-
-list.insertAdjacentHTML('beforeend',html)
-
-})
-
-})
